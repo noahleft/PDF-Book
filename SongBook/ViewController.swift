c9616e7dba630817d8868b3332812297d365a89b
ShihFengLin <noahleft@gmail.com>
2016-11-16 23:00:04 +0800

    Update UI: replace PickerView to TextField with Picker

--- SongBook/ViewController.swift
+++ SongBook/ViewController.swift
@@ -10,21 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var pickerView: UIPickerView!
     @IBOutlet weak var webView: UIWebView!
     @IBOutlet weak var webView2: UIWebView!
+    @IBOutlet weak var pickerTextField: UITextField!
+    
+    
     var songList : [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        pickerView.dataSource = self
-        pickerView.delegate = self
-        
         let songNames = ["song#07", "song#17", "song#18", "song#22", "song#27", "song#29", "song#30", "song#43", "song#48", "song#49", "song#50", "song#54", "song#61", "song#63", "song#66", "song#80", "song#81", "song#86", "song#88", "song#97", "song#102", "song#105", "song#106", "song#108", "song#12", "song#123", "song#125",  "song#131", "song#133"]
         
         songList.append(contentsOf: songNames)
+        
+        let picker = UIPickerView()
+        picker.delegate = self
+        pickerTextField.inputView = picker
+        pickerTextField.text = songList[0]
+        
+        let toolBar = UIToolbar().ToolbarPiker(mySelect: #selector(ViewController.dismissPicker))
+        pickerTextField.inputAccessoryView = toolBar
         
     }
 
@@ -35,9 +42,7 @@ class ViewController: UIViewController {
 
     @IBAction func pressLoadButton(_ sender: AnyObject) {
         
-        let selectedIndex = pickerView.selectedRow(inComponent: 0)
-        let selectedPDF = songList[selectedIndex]
-        print("\(selectedIndex)   \(selectedPDF)")
+        let selectedPDF = pickerTextField.text!
         
         if let pdfURL = Bundle.main.url(forResource: selectedPDF, withExtension: "pdf", subdirectory: nil, localization: nil) {
             print("Phy ya")
@@ -68,6 +73,14 @@ class ViewController: UIViewController {
         
         
     }
+    
+    
+    func dismissPicker() {
+        
+        view.endEditing(true)
+        
+    }
+    
 }
 
 extension ViewController: UIPickerViewDataSource {
@@ -88,5 +101,30 @@ extension ViewController: UIPickerViewDelegate {
         return songList[row]
     }
     
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        pickerTextField.text = songList[row]
+    }
+    
+}
+
+extension UIToolbar {
+    
+    func ToolbarPiker(mySelect : Selector) -> UIToolbar {
+        
+        let toolBar = UIToolbar()
+        
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor.black
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.plain, target: self, action: mySelect)
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        
+        toolBar.setItems([ spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        
+        return toolBar
+    }
     
 }

0b4a0363e5b04638bb98b7226771c86be58a5553
ShihFengLin <noahleft@gmail.com>
2016-12-27 21:51:10 +0800

    initial commit for gear png file initial commit for setting page view controller modify UI

---
+++ SongBook/SettingViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SettingViewController.swift
+//  SongBook
+//
+//  Created by 林世豐 on 27/12/2016.
+//  Copyright © 2016 林世豐. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingViewController: UIViewController {
+    
+    
+    @IBAction func pressClearFileButton(_ sender: Any) {
+        
+        
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    
+    
+}

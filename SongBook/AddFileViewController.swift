fc5214390f7a1225c18cec3fa0b3c12c8bddd862
ShihFengLin <noahleft@gmail.com>
2017-01-11 22:45:02 +0800

    Modify UI initial commit for AddFileViewController

---
+++ SongBook/AddFileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddFileViewController.swift
+//  SongBook
+//
+//  Created by 林世豐 on 11/01/2017.
+//  Copyright © 2017 林世豐. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddFileViewController: UIViewController {
+    
+    
+    
+    
+    
+    
+}
